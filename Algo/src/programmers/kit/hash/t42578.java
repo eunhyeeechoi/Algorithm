5aa653cdd734f7a2031f0ea4925b763f678fb24a
= <=>
2020-11-09 16:21:01 +0900

    hash 문제 풀이 - 프로그래머스 위장

---
+++ Algo/src/programmers/kit/hash/t42578.java
@@ -0,0 +1,17 @@
+package programmers.kit.hash;
+
+//::programmers - HASH 2020.11.09
+//eunhye solution
+public class t42578 {
+	public int solution(String[][] clothes) {
+		int answer = 1;
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		String[][] clothes = { { "yellow_hat", "headgear" }, { "blue_sunglasses", "eyewear" },
+				{ "green_turban", "headgear" } };
+		//경우의 수 인것 같은데
+
+	}
+}
