19209feaa0e716b36c7da79a3b0a96f9f9f4b6d7
= <=>
2020-11-17 23:16:59 +0900

    Bubblesort

---
+++ Algo/src/doit/chap06/BubbleSort2.java
@@ -0,0 +1,24 @@
+package doit.chap06;
+
+public class BubbleSort2 {
+	static void swap(int[] a, int idx1, int idx2) {
+		int t = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = t;
+	}
+
+	static void bubbleSort(int[] a, int n) {
+		for (int i = 0; i < n - 1; i++) {
+			int exchg = 0;// 패스의 교환 횟수를 기록
+			for (int j = n - 1; j > i; j--) {
+				if (a[j - 1] > a[j]) {
+					swap(a, j - 1, j);
+					exchg++;
+				}
+
+				if (exchg == 0)
+					break;// 교환이 이루어지지 않으면 종료
+			}
+		}
+	}
+}
