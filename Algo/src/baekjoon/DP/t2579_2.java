df1e0c63f530c9e5d27c8b9af65faf94c69f8c29
= <=>
2021-01-18 14:57:25 +0900

    DP 계단오르기 복습

--- Algo/src/baekjoon/DP/t2579_2.java
+++ Algo/src/baekjoon/DP/t2579_2.java
@@ -1,13 +1,44 @@
 package baekjoon.DP;
 
-// https://st-lab.tistory.com/132
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// https://sundries-in-myidea.tistory.com/22
-// 백준 DP - 계단오르기
+//백준 DP - 계단오르기
+// https://st-lab.tistory.com/132
+// 재귀 Top-Down 방식
+
 public class t2579_2 {
+	static Integer dp[];
+	static int arr[];
+
 	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		dp = new Integer[N + 1];
+		arr = new int[N + 1];
+
+		for (int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		dp[0] = arr[0]; // 디폴트 값이 null 이므로 0으로 초기화
+		dp[1] = arr[1];
+
+		if (N >= 2) {
+			dp[2] = arr[1] + arr[2];
+		}
+		System.out.println(find(N));
+
+	}
+
+	static int find(int N) {
+		// 아직 탐색하지 않은 N번째 계단일경우
+		if (dp[N] == null) {
+			dp[N] = Math.max(find(N - 2), find(N - 3) + arr[N - 1] + arr[N]);
+		}
+		return dp[N];
 	}
 }
