f8fca6115c9015496fef5778242c3f80eb75db50
= <=>
2021-01-21 15:31:22 +0900

    stack 문제풀이

--- Algo/src/baekjoon/stack/t9012_2.java
+++ Algo/src/baekjoon/stack/t9012_2.java
@@ -3,39 +3,46 @@ package baekjoon.stack;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class t9012 {
+// https://st-lab.tistory.com/178
+public class t9012_2 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
 		int T = Integer.parseInt(br.readLine());
 
-		while (T-- > 0) {
+		for (int i = 0; i < T; i++) {
 			sb.append(solve(br.readLine())).append('\n');
 		}
 		System.out.println(sb);
 	}
 
 	public static String solve(String s) {
-		int count = 0;
-		for (char c : s.toCharArray()) {
-			// 여는 괄호일 경우 카운트 증가
+		Stack<Character> stack = new Stack<>();
+
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+
+			// 여는 괄호일 경우 스택에 넣는다.
 			if (c == '(') {
-				count++;
+				stack.push(c);
 			}
 
-			else if (count == 0) {
+			// 닫는 괄호일 경우
+
+			// 1. 스택이 비어있는 경우, 닫는 괄호를 입력 받았지만 pop 할것이 없을때 짝이 안맞는것
+			else if (stack.empty()) {
 				return "NO";
 			} else {
-				count--;
+				// 2. 그외의 경우 원소를 pop 한다.
+				stack.pop();
 			}
-
-			// 모든 탐색이 끝나고 잔여요소가 있으면 count>0 / 여는 괄호가 만은경우 "NO"
-			// 요소가 비어있으면 count==0 일때는 온전한 수식이므로 "YES"
 		}
-		if (count == 0) {
+
+		if (stack.empty()) {
 			return "YES";
 		} else {
 			return "NO";
---
+++ Algo/src/baekjoon/stack/t9012_3.java
@@ -0,0 +1,24 @@
+package baekjoon.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class t9012_3 {
+	static String[] insert; // 입력되는 문자열 갯수 (괄호덩어리)
+	static String[] out; // 리턴할것 YES, NO
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer Scnt = new StringTokenizer(br.readLine());
+		int Scnt = Integer.parseInt(br.readLine());
+		insert = new String[Scnt]; // 갯수 먼저 꺼내고
+		for (int i = 0; i < Scnt; i++) {
+			insert[i] = br.readLine(); // 각각의 문자열 덩어리 넣고
+//			System.out.println(insert[i]);
+		}
+
+	}
+
+}
