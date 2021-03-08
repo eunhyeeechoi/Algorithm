343c63d10ecabde4e5fa274ff31c2ebec75d47f4
= <=>
2021-03-08 19:11:27 +0900

    리트코드 문제풀이

---
+++ Algo/src/LeetCode/IntersectionofTwoLinkedLists.java
@@ -0,0 +1,25 @@
+package LeetCode;
+
+public class IntersectionofTwoLinkedLists {
+
+//	 * Definition for singly-linked list.
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
+
+	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		ListNode answer = null;
+		return answer;
+
+	}
+
+	public static void main(String[] args) {
+//		ListNode headA = ListNode(4, ListNode);
+	}
+}
