735b3a25778e6734ead23856f7d853c15a468ee9
eunhyeeechoi <ceh070693@gmail.com>
2021-03-29 10:12:21 +0900

    Stack 이용 반복제거

---
+++ Algo/src/LeetCode/Stack/RemoveAllAdjacentDuplicatesInString_2.java
@@ -0,0 +1,32 @@
+package LeetCode.Stack;
+
+import java.util.Stack;
+
+public class RemoveAllAdjacentDuplicatesInString_2 {
+	public static void main(String[] args) {
+		String S = "abbaca";
+		System.out.println(removeDuplicates(S));
+
+	}
+
+	public static String removeDuplicates(String S) {
+		char[] chr = S.toCharArray();
+		Stack<Character> str = null;
+		for (int i = 0; i < S.length(); i++) {
+			if (str.isEmpty()) {
+				// 비어있으면
+				str.push(chr[i]);
+				continue;
+			} else {
+				// 비어있지 않으면
+				if (str.peek() == chr[i]) {
+					str.pop();
+					continue;
+				} else {
+					str.push(chr[i]);
+				}
+			}
+		}
+		return str.toString();
+	}
+}
